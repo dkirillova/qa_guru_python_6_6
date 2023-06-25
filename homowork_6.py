1c5098cec0123dd28fde7b8f2d51fc95fd8dde03
Daria Kirillova <dasha.kirillova.9614@gmail.com>
2023-06-25 14:52:03 +0300

    исправлен def test_dark_theme_by_time_and_user_choice

--- homowork_6.py
+++ homowork_6.py
@@ -23,20 +23,17 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
     is_dark_theme = None
-    if time(hour=6) < current_time < time(hour=22):
-        is_dark_theme = False
-    if time(hour=7) < current_time < time(hour=21):
-        is_dark_theme = True
 
-    if dark_theme_enabled_by_user == True:
-        is_dark_theme = True
-    elif dark_theme_enabled_by_user == False:
-        is_dark_theme = False
+    if dark_theme_enabled_by_user is None:
+        if 7 < current_time.hour >= 19:
+            is_dark_theme = True
+    else:
+        is_dark_theme = dark_theme_enabled_by_user
 
     assert is_dark_theme is True
 
